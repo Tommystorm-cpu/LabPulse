7fb4fb65a1b0886ed370afab3edf36f510632a2e
JBond2004 <jdbond1507@icloud.com>
2025-06-16 10:39:42 +0100

    Finished first draft of code

--- Arduino code/Arduino Instructions for TH sensor.cpp
+++ Arduino code/Arduino Instructions for TH sensor.cpp
@@ -1,17 +1,39 @@
-#include "TH_sensor_code.cpp"
+/*
+Code for the Arduino to copy and paste into the IDE. 
 
-TH_sensor_code DHT;
+This code runs the TH_sensor_code.cpp file assuming it has been imported
+as a git file
+*/
 
+#include "pathname.h"
+
+pathname DHT;
+
+//Pin number, assuming it is pin D2, but should be changed if otherwise
+int pin = 2;
+//Sets up the baud rate to be 9600
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  int chk = DHT.read(2);  // Reads sensor on pin D2
-  Serial.print("Temperature: ");
-  Serial.print(DHT.temperature);
-  Serial.print(" °C, Humidity: ");
-  Serial.print(DHT.humidity);
-  Serial.println(" %");
-  delay(2000);
+    int check = DHT.read(pin);
+    
+    if (check == 0) {
+        Serial.print("Temperature: ");
+        Serial.print(DHT.temperature);
+        Serial.print(" °C, Humidity: ");
+        Serial.print(DHT.humidity);
+        Serial.println(" %");
+        delay(2000);
+    }
+    if (check == -1) {
+        Serial.print("Checksum Error");
+        delay(2000);
+    }
+    if (check == -2) {
+        Serial.print("Timeout Error");
+        delay(2000);
+    }
+
 }
