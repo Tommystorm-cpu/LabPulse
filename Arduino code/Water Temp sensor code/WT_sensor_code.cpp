d20c10f7ff194ef5a2a3506bbdad0e31f3df7ee8
JBond2004 <jdbond1507@icloud.com>
2025-06-23 14:43:37 +0100

    Finished WT_sensor_code.cpp and added comments

--- Arduino code/Water Temp sensor code/WT_sensor_code.cpp
+++ Arduino code/Water Temp sensor code/WT_sensor_code.cpp
@@ -4,6 +4,8 @@ Code to generate the temperature of a water temperature sensor from a resistor
 This code should be either downloaded into the Arduino IDE as a .cpp
 file or copy and pasted into one.
 */
+//include math.h module to use the natural log
+#include <math.h>
 
 //Checks that the ARDUINO is defined and is version 1.0.0 or greater
 //If it is it uses the Arduino.h program, if not it uses a legacy program 
@@ -24,11 +26,17 @@ class WT_sensor {
       float D,
       float Res_const
       );
-        float Temperature;
-}
+    float Temperature;
+    float Res_sensor;
+};
 
 /*
-Function from WT_sensor class called read.
+Function from WT_sensor class called read, its inputs are the pin number to use
+from A0-A5, the Steinhart-Hart parameters A,B,C and D and the resistance of the 
+constant resistor in the voltage divider circuit.
+
+This function outputs the temperature output of the sensor, but also the resistance
+of the sensor for testing.
 */
 float WT_sensor::read(int pin,
                       float A,
@@ -38,11 +46,15 @@ float WT_sensor::read(int pin,
                       float Res_const){
   //Read voltage across sensor, assuming the supply voltage is 5.0V and 10-bit ADC
   float voltage = analogRead(pin) * (5.0 / 1023.0);
-  float res_cont_voltage = 5.0 - voltage;
+  //Total supply is 5.0V, hence the constant resistor must have this voltage
+  float res_const_voltage = 5.0 - voltage;
 
-  float Res_sensor = (voltage/res_const_voltage) * Res_const;
-  float lnR = log (Res_sensor)
-  Temperature = (A + B * (lnR) + C * (lnR ** 2) + D * (lnR ** 3)) ** -1;
+  //Calculate the sensors resistance
+  Res_sensor = (voltage/res_const_voltage) * Res_const;
+  float lnR = log (Res_sensor);
 
-  return Temperature
+  //Steinhart-Hart equation - converted to celcius
+  Temperature = (1.0 / (A + B * (lnR) + C * pow(lnR,2) + D * pow(lnR,3))) - 273.15;
+  
+  return Temperature;
 }
