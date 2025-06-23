bd75a28cd641a1edc8220d5bdcf0c18ef05a0073
JBond2004 <jdbond1507@icloud.com>
2025-06-23 14:38:19 +0100

    Added comments and finished code

--- Arduino code/Water Temp sensor code/Arduino Instructions for WT sensor.cpp
+++ Arduino code/Water Temp sensor code/Arduino Instructions for WT sensor.cpp
@@ -1,37 +1,49 @@
 #include "WT_sensor_code.h"
-#include <list>
 
-T_sens WT_sensor;
+WT_sensor T_sens;
 
 //Define all pins used - up to 6 analog pins available
-list<int> pins = {A0};
-int num_pins = pins.size()
+//Note that you must change the i value in the loop() to 
+//match the number of entries in the list
+int pins[2] = {A0,A1};
 
+//Steinhart-Hart parameters determined by python curve_fitting
 float A = 0.0014948;
 float B = 0.00021902;
 float C = 1.6239e-6;
 float D = 3.4445e-8;
-float Res_const = 2000;
+//Resistance of the constant resistor used
+float Res_const = 2200;
 
 void setup() {
   Serial.begin(9600); // Start serial communication
 }
 
-int i = 0
+//Initialise on pin 0, and check each pin's output
+int i = 0;
 void loop() {
+  //Generate the temperature output of a pin
   float Temperature = T_sens.read(pins[i],
   A,
   B,
   C,
   D,
   Res_const);
-
+  //Print the temperature/resistance output 
+  //Use resistance for testing circuit with known resistors.
   Serial.print("Temperature across pin ");
   Serial.print(i);
+  Serial.print(" ");
   Serial.println(Temperature);
+  /*Serial.print("Sensor Resistance of sensor ");
+  Serial.print(i);
+  Serial.print(" ");
+  Serial.println(T_sens.Res_sensor);*/
 
+  //Move to the next pin
   i++;
-  if (i == num_pins) {
+  //If every pin has been read, wait 5 seconds and start again
+  if (i == 2) {
     i = 0;
     delay(5000);
   }
