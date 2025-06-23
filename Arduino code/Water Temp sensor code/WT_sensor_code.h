ade9c333fc8c1254c01576aae6b6afab2df1e0f3
JBond2004 <jdbond1507@icloud.com>
2025-06-23 12:06:13 +0100

    Create WT_sensor_code.h and its first draft

---
+++ Arduino code/Water Temp sensor code/WT_sensor_code.h
@@ -0,0 +1,27 @@
+/*
+Header file to run the water temperature sensor on an Arduino
+*/
+
+#ifndef WT_sensor_code_H
+#define WT_sensor_code_H
+
+// Check Arduino version
+#if defined(ARDUINO) && (ARDUINO >= 100)
+  #include <Arduino.h>
+#else
+  #include <WProgram.h>
+#endif
+
+class WT_sensor {
+  public:
+    float read(int pin,
+      float A,
+      float B,
+      float C,
+      float D,
+      float Res_const
+      );
+        float Temperature;
+};
+
+#endif
