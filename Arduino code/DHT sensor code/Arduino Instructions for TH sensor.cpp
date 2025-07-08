6f6a165166188919321e77e82baaaf6d82b1226d
JBond2004 <jdbond1507@icloud.com>
2025-07-08 13:43:58 +0100

    Added comments and error statement

--- Arduino code/DHT sensor code/Arduino Instructions for TH sensor.cpp
+++ Arduino code/DHT sensor code/Arduino Instructions for TH sensor.cpp
@@ -1,27 +1,32 @@
+//Include the dht11 class and define an object of that class
 #include "TH_sensor_code.h"
-
 dht11 DHT;
-int pin_num = 2; // Replace 2 with the correct pin
+
+//Digital pin number
+int pin_num = 2; 
 
 void setup() {
-  Serial.begin(9600); // Start serial communication
+  Serial.begin(9600); 
 }
 
 void loop() {
+  //Use DHT.read function, it returns temperature and humidity
   int result = DHT.read(pin_num);
 
+  //If no errors return print results
   if (result == DHTLIB_OK) {
     Serial.print("Temp: ");
     Serial.print(DHT.temperature);
     Serial.print("°C  Hum: ");
     Serial.print(DHT.humidity);
     Serial.println("%");
-  } 
-  if (result == DHTLIB_ERROR_TIMEOUT) {
+  } else if (result == DHTLIB_ERROR_TIMEOUT) {
     Serial.print("Timeout Error");
-  }
-  if (result == DHTLIB_ERROR_CHECKSUM) {
+  } else if (result == DHTLIB_ERROR_CHECKSUM) {
     Serial.print("CHECKSUM error");
+  } else {
+    Serial.print("Something has gone wrong")
   }
+  //Wait 2 seconds before next reading
   delay(2000);
 }
