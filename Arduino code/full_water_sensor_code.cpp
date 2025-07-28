ec9f220b329e5794c8b3c86a755c359f39fb45ea
JBond2004 <jdbond1507@icloud.com>
2025-07-28 11:01:39 +0100

    Fixed full_water_sensor_code and made it more modular

--- Arduino code/full_water_sensor_code.cpp
+++ Arduino code/full_water_sensor_code.cpp
@@ -1,92 +1,135 @@
 #include <Arduino.h>
 
-// --- Temperature Sensor Setup ---
-const int analogPins[4] = {A0, A1, A2, A3};
-const float seriesResistor = 10000; // 10k?
-const float nominalResistance = 10000; // Thermistor resistance at 25ï¿½C
-const float nominalTemperature = 25;
-const float bCoefficient = 3950;
-const int adcMax = 1023;
+int pins[4] = {A0,A1,A2,A3};
 
-float readTemperature(int adcValue) {
-  float voltage = adcValue / (float)adcMax;
-  float resistance = seriesResistor * (1.0 / voltage - 1.0);
+//Steinhart-Hart parameters determined by python curve_fitting
+float A = 0.0014948;
+float B = 0.00021902;
+float C = 1.6239e-6;
+float D = 3.4445e-8;
+//Resistance of the constant resistor used
+float Res_const = 4700;
 
-  float steinhart;
-  steinhart = resistance / nominalResistance;
-  steinhart = log(steinhart);
-  steinhart /= bCoefficient;
-  steinhart += 1.0 / (nominalTemperature + 273.15);
-  steinhart = 1.0 / steinhart;
-  steinhart -= 273.15;
+byte sensorPin_1 = 2;
+const byte sensorInterrupt_1 = digitalPinToInterrupt(sensorPin_1);
+byte sensorPin_2 = 3;
+const byte sensorInterrupt_2 = digitalPinToInterrupt(sensorPin_2);
 
-  return steinhart;
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+
+volatile byte pulseCount_1;
+volatile byte pulseCount_2;
+
+float flowRate_1;
+unsigned long flowMilliLitres;
+unsigned long totalMilliLitres;
+
+float flowRate_2;
+unsigned long flowMilliLitres_2;
+unsigned long totalMilliLitres_2;
+
+unsigned long oldTime;
+
+float readTemperature(int voltage) {
+  //Total supply is 5.0V, hence the constant resistor must have this voltage
+  float res_const_voltage = 5.0 - voltage;
+  //Calculate the sensors resistance
+  float Res_sensor = (voltage/res_const_voltage) * Res_const;
+  float lnR = log(Res_sensor);
+
+  //Steinhart-Hart equation - converted to celcius
+  float Temperature = (1.0 / (A + B * (lnR) + C * pow(lnR,2) + D * pow(lnR,3))) - 273.15;]
+  return Temperature
 }
 
-// --- Flow Sensor Setup ---
-volatile int flowCount1 = 0;
-volatile int flowCount2 = 0;
-unsigned long lastTime = 0;
-float calibrationFactor = 4.5; // Pulses per second per L/min
-float totalLitres1 = 0;
-float totalLitres2 = 0;
-
-void flow1ISR() {
-  flowCount1++;
+//Each turn of the water flow sensors wheel interrupts the program
+//and adds on 1 to the pulseCount
+void pulseCounter_1() {
+  pulseCount_1++;
 }
 
-void flow2ISR() {
-  flowCount2++;
+void pulseCounter_2() {
+  pulseCount_2++;
+}
+
+void printResults() {
+  detachInterrupt(sensorInterrupt_1);
+  detachInterrupt(sensorInterrupt_2);
+  
+  flowRate_1 = ((1000.0 / (millis() - oldTime)) * pulseCount_1) / calibrationFactor;
+  flowRate_2 = ((1000.0 / (millis() - oldTime)) * pulseCount_2) / calibrationFactor;
+  
+  totalLitres_1 += flowRate_1 / 60;
+  totalLitres_2 += flowRate_2 / 60;
+      
+  Serial.print("Flow1: ");
+  Serial.print(flowRate_1, 2);
+  Serial.print(" L/min, Total1: ");
+  Serial.print(totalLitres_1, 2);
+  Serial.print(" L | ");
+  
+  Serial.print("Flow2: ");
+  Serial.print(flowRate_2, 2);
+  Serial.print(" L/min, Total2: ");
+  Serial.println(totalLitres_2, 2);
+      
+  // --- Temperature Readings ---
+  for (int i = 0; i < 4; i++) {
+    float voltage = analogRead(pins[i]);
+    float Temperature = readTemperature(voltage);
+    Serial.print("Temperature across pin ");
+    Serial.print(i);
+    Serial.print("is ");
+    Serial.print(Temperature);
+    Serial.println("Celcius")
+    }
 }
 
 void setup() {
+  
+  // Initialize a serial connection for reporting values to the host
   Serial.begin(9600);
 
-  // Flow sensor interrupts
-  attachInterrupt(digitalPinToInterrupt(2), flow1ISR, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), flow2ISR, RISING);
+  //Set input pin to accept signal
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
 
-  lastTime = millis();
+  //Initialise variables
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+
+  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(sensorInterrupt_1, pulseCounter_1, FALLING);
+  attachInterrupt(sensorInterrupt_2, pulseCounter_2, FALLING);
 }
 
 void loop() {
-  unsigned long currentTime = millis();
+  /* The Hall-effect sensor provides pulses corresponding to 4.5 per second per
+  litre/min of flow. Since pulse count is stored as a byte, we can only store 255 
+  pulses, so we must only count for every second.
+  */
+  int timecounter = 0;
+  if (millis() - oldTime >= 1000) {
+    timecounter++;
 
-  if (currentTime - lastTime >= 2000) {  // Every 2 seconds
-    // --- Temperature Readings ---
-    for (int i = 0; i < 4; i++) {
-      int adcValue = analogRead(analogPins[i]);
-      float tempC = readTemperature(adcValue);
-      Serial.print("Temp");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.print(tempC, 2);
-      Serial.print(" ï¿½C  ");
+    if (timecounter == 3) {
+      printResults();
+      timecoutner = 0;
     }
-    Serial.println();
-
-    // --- Flow Rate Readings ---
-    float flowRate1 = (flowCount1 / calibrationFactor); // L/min
-    float flowRate2 = (flowCount2 / calibrationFactor);
-
-    totalLitres1 += flowRate1 / 60.0 * 2.0; // 2 seconds interval
-    totalLitres2 += flowRate2 / 60.0 * 2.0;
-
-    Serial.print("Flow1: ");
-    Serial.print(flowRate1, 2);
-    Serial.print(" L/min, Total1: ");
-    Serial.print(totalLitres1, 2);
-    Serial.print(" L | ");
-
-    Serial.print("Flow2: ");
-    Serial.print(flowRate2, 2);
-    Serial.print(" L/min, Total2: ");
-    Serial.println(totalLitres2, 2);
-    
-    // Reset counters
-    flowCount1 = 0;
-    flowCount2 = 0;
-    lastTime = currentTime;
+     // Reset counters
+    pulseCount_1 = 0;
+    pulseCount_2 = 0;
+    oldTime = millis();
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(sensorInterrupt_1, pulseCounter_1, FALLING);
+    attachInterrupt(sensorInterrupt_2, pulseCounter_2, FALLING);
   }
 }
 
